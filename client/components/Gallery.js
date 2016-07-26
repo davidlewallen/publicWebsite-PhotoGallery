b484eeaffafe388b44311c69438c2ab6514891e3
davidlewallen <lewallen.david@gmail.com>
2016-07-25 23:47:33 -0500

    Started working on the Gallery component

---
+++ client/components/Gallery.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+export default class Gallery extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {}
+	}
+
+	render() {
+		return (
+			<div className="gallery-component">
+				<p> does this even work?</p>
+			</div>
+		)
+	}
+}
